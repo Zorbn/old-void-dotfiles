f3edba592a76766ee3d46240537494c98d501243
Zorbn <public.zorb@gmail.com>
2021-11-28 11:29:56 -0800

    Switched to nvim-lsp-installer

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,7 +1,15 @@
 -- Setup LSP integration
 
-local pid = vim.fn.getpid()
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lsp_installer = require("nvim-lsp-installer")
+
+lsp_installer.on_server_ready(function(server)
+    local opts = { capabilities = capabilities }
+    server:setup(opts)
+end)
+
+--[[
+local pid = vim.fn.getpid()
 local lsp = require 'lspconfig'
 
 lsp.omnisharp.setup {
@@ -13,3 +21,4 @@ lsp.omnisharp.setup {
 }
 
 lsp.rust_analyzer.setup { capabilities = capabilities }
+--]]
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -41,6 +41,7 @@ Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'saadparwaiz1/cmp_luasnip'
 Plug 'L3MON4D3/LuaSnip'
+Plug 'williamboman/nvim-lsp-installer'
 
 Plug('pradyungn/Mountain', { ['rtp'] = 'vim' })
 
